8939643457e61be695b7fe9a0e3ae21e9950394b
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-06-28 23:38:05 +0530

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import Head from "next/head";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <title>EngimaPaste</title>
+      <Head>
+        <title>EngimaPaste</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
