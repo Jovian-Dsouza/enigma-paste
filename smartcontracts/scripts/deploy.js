ffe6034fe5244d0e26e099118999fc0fcdbbe51a
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-06-29 11:35:23 +0530

    Add EnigmaPaste smartcontract

--- smartcontracts/scripts/deploy.js
+++ smartcontracts/scripts/deploy.js
@@ -1,17 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs");
+
+function updateContractAddress(contractAddressFile, address) {
+  const addressLine = "ENIGMAPASTE_ADDRESS=";
+  if (fs.existsSync(contractAddressFile)) {
+    let fileContents = fs.readFileSync(contractAddressFile, "utf8");
+    const addressLineIndex = fileContents.indexOf(addressLine);
+    if (addressLineIndex !== -1) {
+      const start = addressLineIndex + addressLine.length;
+      const end = fileContents.indexOf("\n", start);
+      const existingAddress = fileContents.substring(start, end);
+      fileContents = fileContents.replace(existingAddress, address);
+    } else {
+      fileContents += `${addressLine}${address}\n`;
+    }
+    fs.writeFileSync(contractAddressFile, fileContents);
+  } else {
+    fs.writeFileSync(contractAddressFile, `${addressLine}${address}\n`);
+  }
+  console.log(`Contract address updated in ${contractAddressFile}`);
+}
 
 async function main() {
+  const network = hre.network.name;
+  console.log("Deploying to network:", network);
+
   const contract = await hre.ethers.deployContract("EnigmaPaste");
-
   await contract.waitForDeployment();
+  const contract_address = await contract.getAddress();
+  console.log(`Contract deployed at address: ${contract_address}`);
 
-  console.log(`Contract deployed`);
+  if (network === "goerli") {
+    updateContractAddress("../.env.local", contract_address);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
