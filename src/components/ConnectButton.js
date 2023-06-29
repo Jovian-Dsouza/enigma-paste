1ff948249ac8fe71b3edb52a382ac05e25b1e536
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-06-29 18:36:48 +0530

    Update connect button to use thirdweb wallet

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,45 +1,13 @@
-import { useEffect, useState } from "react";
-import {
-  useAccount,
-  useConnect,
-  useDisconnect,
-} from "wagmi";
+import { ConnectWallet } from "@thirdweb-dev/react";
 
-export function ConnectButton({ className }) {
-  const [buttonTxt, setButtonTxt] = useState('Loading...')
-  const { address, isConnected } = useAccount();
-  const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect();
-  const { disconnect } = useDisconnect();
-
-  const [connector] = connectors;
-
-  useEffect((()=>{
-    if(isConnected){
-        setButtonTxt('Disconnect')
-    }
-    else{
-        setButtonTxt('Connect')
-    }
-  }), [isConnected]);
-
-  function handleToggleConnect({connector}) {
-    if(isConnected){
-        disconnect();
-        setButtonTxt('Connect');
-    }
-    else{
-        connect({connector});
-        setButtonTxt("Disconnect");
-    }
-  }
-
-  return (
-    <a
-      onClick={() => handleToggleConnect({ connector })}
-      className={`text-brownishBlack hover:text-white hover:bg-brownishBlack bg-white border-white font-bold border-2 px-4 py-2 rounded-lg ${className}`}
-    >
-      {buttonTxt}
-    </a>
-  );
+export function ConnectButton({ className }){
+    return (
+      <div className={`${className} border-2 border-white rounded-xl`}>
+        <ConnectWallet
+          theme="dark"
+          className="!h-12 !font-bold"
+          btnTitle="Connect"
+        />
+      </div>
+    );
 }
