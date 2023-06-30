c60e365d47936a8597e3576d1ec2cb4d0f1269a7
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-06-30 22:47:40 +0530

    Update about project page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,35 @@
-import { UnderConstruction } from "@/components/UnderConstruction";
 export default function AboutPage() {
-  return <UnderConstruction />;
+  return (
+    <div className="flex flex-col h-screen justify-start items-center py-20 px-10">
+      <div className="space-y-10">
+        <div className="max-w-lg text-3xl font-bold text-center">
+          Unleash Your Words 📝, Embrace Decentralization 🌐
+        </div>
+        <div className="space-y-10 max-w-xl mx-auto text-md">
+          <p className="">
+            Empowering Privacy-Conscious Creators. Store, Share & Protect Your
+            Textual Gems with Blockchain Security. Create Encrypted Pastes for
+            Secure Data Storage. Take Control of Your Content Today. Join the
+            Decentralized Text Revolution. Safeguard Your Ideas with Enigma
+            Paste - Unlock the Future of Privacy-Driven Collaboration.
+          </p>
+          <p>
+            Join the Enigma Paste community today and experience the future of
+            decentralized text storage. Start securely storing and sharing your
+            code snippets, notes, and textual content with peace of mind.
+          </p>
+          <p>
+            If you have any questions, suggestions, or feedback, feel free to
+            reach out to us at{" "}
+            <span>
+              <a href="mailto:dsouzajovian123@gmail.com">
+                dsouzajovian123@gmail.com
+              </a>
+            </span>
+            . We'd love to hear from you!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
